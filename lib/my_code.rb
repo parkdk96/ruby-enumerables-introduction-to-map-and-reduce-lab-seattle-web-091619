64d5712607b498a3c4b3f72df145fd95a6537322
Daniel Park <parkdk96@gmail.com>
2019-08-24 03:52:14 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -32,6 +32,10 @@ def reduce_to_total(source_array, starting_point = 0)
 end
 
 def reduce_to_all_true(source_array)
+  
+end
+
+def reduce_to_any_true(source_array)
   source_array.length.times do |index|
     if !!source_array[index]
       return true
@@ -39,6 +43,4 @@ def reduce_to_all_true(source_array)
   end
   return true
 end
-
-def reduce_to_any_true(source_array)
   
